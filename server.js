68cd21942d0055a72eba52240688010714af38f0
Serhat Yildiz <yildizserhat@yahoo.com>
2024-06-24 15:54:14 +0300

    Final deployment prep

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 require('dotenv').config()
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-
+const path = require('path')
 
 // importing models
 const userModel = require('./models/userModel')
@@ -725,6 +725,26 @@ app.delete("/users/:userId/startdate", verifyToken, async (req, res) => {
         res.status(500).json({ message: "Error deleting start date" });
     }
 });
+
+if (process.env.NODE_ENV === "production") {
+    // Serve the static files from the Vite build directory
+    app.use(express.static(path.join(__dirname, '/client/dist')));
+  
+    // Serve the JavaScript file
+    app.get('/assets/index-CW5KkyhG.js', (req, res) => {
+      res.sendFile(path.join(__dirname, '/client/dist/assets/index-CW5KkyhG.js'));
+    });
+  
+    // Handle all other routes by serving the index.html file
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '/client/dist/index.html'));
+    });
+  } else {
+    // In development mode, just return a simple message
+    app.get('/', (req, res) => {
+      res.send('API running');
+    });
+  }
 
 
 
